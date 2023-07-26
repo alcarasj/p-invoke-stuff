79ec95ed3bf6de9032b757fc399e4c40a30105f9
Jerico Alcaras <jalcaras@microsoft.com>
2023-07-26 15:37:13 +0100

    Cleanup C++ code.

--- NativeStuff/NativeStuff.cpp
+++ NativeStuff/NativeStuff.cpp
@@ -1,5 +1,6 @@
 ﻿#include "NativeStuff.h"
-#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 __declspec(dllexport) char * say_hello(char * name)
 {
--- NativeStuff/NativeStuff.h
+++ NativeStuff/NativeStuff.h
@@ -1,15 +1,3 @@
-﻿#include <comutil.h>
-#ifndef _NATIVESTUFF_H_
-#define _NATIVESTUFF_H_
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-__declspec(dllexport) char * say_hello(char * name);
-
-#ifdef __cplusplus
+﻿extern "C" {
+  __declspec(dllexport) char * say_hello(char * name);
 }
-#endif
-
-#endif _NATIVESTUFF_H_
